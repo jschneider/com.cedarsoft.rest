17f74314c2ff736714ce9749922bc96626978d45
Johannes Schneider <js@cedarsoft.com>
2010-08-16 17:06:42 +0200

    Generator: Setting href field

--- generator/src/main/java/com/cedarsoft/rest/generator/TestGenerator.java
+++ generator/src/main/java/com/cedarsoft/rest/generator/TestGenerator.java
@@ -32,11 +32,11 @@
 package com.cedarsoft.rest.generator;
 
 import com.cedarsoft.codegen.CodeGenerator;
-import com.cedarsoft.codegen.DecisionCallback;
 import com.cedarsoft.codegen.NamingSupport;
 import com.cedarsoft.codegen.model.DomainObjectDescriptor;
 import com.cedarsoft.codegen.model.FieldWithInitializationInfo;
 import com.cedarsoft.rest.AbstractJaxbTest;
+import com.cedarsoft.rest.JaxbTestUtils;
 import com.sun.codemodel.JClass;
 import com.sun.codemodel.JClassAlreadyExistsException;
 import com.sun.codemodel.JDefinedClass;
@@ -45,13 +45,21 @@ import com.sun.codemodel.JExpression;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JMod;
 import com.sun.codemodel.JVar;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @param <T> the type of the decision callback
  */
-public class TestGenerator extends AbstractGenerator<JaxbObjectGenerator.MyDecisionCallback > {
-  public TestGenerator( @NotNull CodeGenerator<JaxbObjectGenerator.MyDecisionCallback > codeGenerator, @NotNull DomainObjectDescriptor descriptor ) {
+public class TestGenerator extends AbstractGenerator<JaxbObjectGenerator.MyDecisionCallback> {
+  @NonNls
+  public static final String METHOD_NAME_SET_HREF = "setHref";
+  @NonNls
+  public static final String METHOD_NAME_CREATE_TEST_URI_BUILDER = "createTestUriBuilder";
+  @NonNls
+  public static final String METHOD_NAME_BUILD = "build";
+
+  public TestGenerator( @NotNull CodeGenerator<JaxbObjectGenerator.MyDecisionCallback> codeGenerator, @NotNull DomainObjectDescriptor descriptor ) {
     super( codeGenerator, descriptor );
   }
 
@@ -69,6 +77,9 @@ public class TestGenerator extends AbstractGenerator<JaxbObjectGenerator.MyDecis
       JMethod method = testClass.method( JMod.PUBLIC, jaxbClass, "createObjectToSerialize" )._throws( Exception.class );
       method.annotate( Override.class );
       JVar field = method.body().decl( jaxbClass, "object", JExpr._new( jaxbClass ) );
+
+      //Sets the href
+      method.body().add( field.invoke( METHOD_NAME_SET_HREF ).arg( codeGenerator.ref( JaxbTestUtils.class ).staticInvoke( METHOD_NAME_CREATE_TEST_URI_BUILDER ).invoke( METHOD_NAME_BUILD ) ) );
 
       //Sets the values
       for ( FieldWithInitializationInfo fieldInfo : descriptor.getFieldsToSerialize() ) {
