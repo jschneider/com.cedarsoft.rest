3e4ca0904811385a98760c3c150a225e80767fea
Johannes Schneider <js@cedarsoft.com>
2010-08-20 14:43:38 +0200

    JaxbInheritanceTest added

---
+++ sample/src/test/java/com/cedarsoft/rest/sample/jaxb/JaxbInheritanceTest.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (C) cedarsoft GmbH.
+ *
+ * Licensed under the GNU General Public License version 3 (the "License")
+ * with Classpath Exception; you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *         http://www.cedarsoft.org/gpl3ce
+ *         (GPL 3 with Classpath Exception)
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation. cedarsoft GmbH designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by cedarsoft GmbH in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 3 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 3 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact cedarsoft GmbH, 72810 Gomaringen, Germany,
+ * or visit www.cedarsoft.com if you need additional information or
+ * have any questions.
+ */
+
+package com.cedarsoft.rest.sample.jaxb;
+
+import com.cedarsoft.AssertUtils;
+import com.cedarsoft.jaxb.AbstractJaxbObject;
+import com.cedarsoft.rest.JaxbRule;
+import org.junit.*;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class JaxbInheritanceTest {
+  @Rule
+  public JaxbRule jaxbRule = new JaxbRule( SuperClass.class, SubClass.class );
+
+  @Test
+  public void testSuper() throws Exception {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    jaxbRule.createMarshaller().marshal( new SuperClass(), out );
+
+    AssertUtils.assertXMLEquals(
+      "<ns2:daObject xmlns:ns2=\"daobject/super\" xmlns:ns3=\"daobject/super\">\n" +
+        "  <id>7</id>\n" +
+        "</ns2:daObject>", out.toString() );
+
+    Object deserialized = jaxbRule.createUnmarshaller().unmarshal( new ByteArrayInputStream( out.toByteArray() ) );
+    assertEquals( SuperClass.class, deserialized.getClass() );
+  }
+
+  @Test
+  public void testSub() throws Exception {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    jaxbRule.createMarshaller().marshal( new SubClass(), out );
+
+    AssertUtils.assertXMLEquals(
+      "<ns3:daObject xmlns:ns3=\"daobject/sub\" xmlns:ns2=\"daobject/sub\">\n" +
+        "  <id>7</id>\n" +
+        "  <daString>asdf</daString>\n" +
+        "</ns3:daObject>", out.toString() );
+
+    Object deserialized = jaxbRule.createUnmarshaller().unmarshal( new ByteArrayInputStream( out.toByteArray() ) );
+    assertEquals( SubClass.class, deserialized.getClass() );
+  }
+
+  @XmlRootElement( name = "daObject", namespace = "daobject/super" )
+  @XmlAccessorType( XmlAccessType.FIELD )
+  public static class SuperClass extends AbstractJaxbObject {
+    private final int id = 7;
+
+  }
+
+  @XmlRootElement( name = "daObject", namespace = "daobject/sub" )
+  @XmlAccessorType( XmlAccessType.FIELD )
+  public static class SubClass extends SuperClass {
+    private final String daString = "asdf";
+  }
+}
